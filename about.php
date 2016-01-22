6981dd2e3bd7077a329510de31dcb8a91c13702a
Chikaysci <kjabellar@gmail.com>
2016-01-22 09:48:42 +0800

    about page

---
+++ about.php
@@ -0,0 +1,282 @@
+<?php include 'header-pages.php'; ?>
+
+<title>About <?=$domain?></title>
+	<meta name="title" content="About - <?=$domain?> " />
+	<meta name="description" content="About <?=$domain?> today." />
+	<style>
+	.full-screen {
+		min-height:459px !important;
+	}
+	</style>
+    </head>
+<body>
+<? include 'navigation.php'; ?>
+    
+      <section class="content-top-margin page-title page-title-small bg-gray">
+            <div class="container">
+                <div class="row">
+                    <div style="visibility: visible; animation-duration: 300ms; animation-name: fadeInUp;" class="col-md-8 col-sm-12 wow fadeInUp animated" data-wow-duration="300ms">
+                        <!-- page title -->
+                        <h1 class="black-text">About <?=$domain?></h1>
+                        <!-- end page title -->
+                    </div>
+                    <div style="visibility: visible; animation-duration: 600ms; animation-name: fadeInUp;" class="col-md-4 col-sm-12 breadcrumb text-uppercase wow fadeInUp xs-display-none animated" data-wow-duration="600ms">
+                        <!-- breadcrumb -->
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/about">About</a></li>
+                        </ul>
+                        <!-- end breadcrumb -->
+                    </div>
+                </div>
+            </div>
+        </section>		   
+	<section style="visibility: visible; animation-name: fadeIn;" class="bg-black wow fadeIn animated">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-8 text-center center-col">
+                        <div class="about-year text-uppercase white-text"><span class="clearfix">20</span> Years</div>
+                        <p class="title-small text-uppercase letter-spacing-1 white-text font-weight-100">We have been in the forefront of domain development and technologies since 1996</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+			<section style="visibility: visible; animation-name: fadeIn;" class="wow fadeIn animated">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-sm-12 center-col">
+                        <!-- tab -->
+                        <div class="tab-style1">
+                            <div class="row">
+                                <div class="col-md-12 col-sm-12">
+                                    <!-- tab navigation -->
+                                    <ul class="nav nav-tabs nav-tabs-light text-center height-auto">
+                                        <li class=""><a href="#tab_sec1" data-toggle="tab">About ModelTable</a></li>
+                                        <li class=""><a href="#tab_sec2" data-toggle="tab">Where and How </a></li>
+                                        <li class="active"><a href="#tab_sec3" data-toggle="tab">Partners</a></li>
+                                    </ul>
+                                    <!-- tab end navigation -->
+                                </div>
+                            </div>
+                            <div class="row"> 
+                                <div class="col-md-12 col-sm-12">
+                                    <div class="wide-separator-line no-margin-lr"></div>
+                                </div>
+                            </div>
+                            <!-- tab content section -->
+                            <div class="tab-content">
+                                <!-- tab content -->
+                                <div class="tab-pane med-text fade" id="tab_sec1">
+                                    <div class="row"> 
+                                        <div class="col-md-6 col-sm-12 text-left gray-text">
+                                            <p class="text-large margin-right-ten">Veterans Rehab is a progressive and agile program and system for effective rehabilitation for todays Veterans.  </p>
+                                        </div>
+                                        <div class="col-md-6 col-sm-12 text-left text-med gray-text">
+                                            <p class="text-uppercase">Both physical and mental rehabilitation is needed for a vast number of veterans.   </p>
+                                            <p>We are pushing the boundaries of using and creating new technologies that can measure results, create collaborative and safe environment with a network of professionals focused on the well being of todays Veterans.</p>
+                                        </div>
+                                    </div>
+                                    <div class="row"> 
+                                        <div class="col-md-12 col-sm-12">
+                                            <div class="wide-separator-line no-margin-lr"></div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 col-sm-12 text-center service-year black-text">
+                                            <strong>Digital development for 20 years</strong>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- end tab content -->
+                                <!-- tab content -->
+                                <div class="tab-pane fade" id="tab_sec2">
+                                    <div class="row"> 
+                                        <div class="col-md-6 col-sm-12 text-left gray-text">
+                                            <p class="text-large margin-right-ten">Veterans need help and we have to be there when they need it.</p>
+                                        </div>
+                                        <div class="col-md-6 col-sm-12 text-left text-med gray-text">
+                                            <p class="text-uppercase">The custom programs we create allow for greater individual growth of our Veterans while utilizing Virtual Reality, BioSensoring, Dietary Development and Management along with education that includes are amazing Military Degree program for memebers of our Veterans Rehab facility. </p>
+                                            <p>Using a well know estate location, Kessler Mansion, we bring together the right people and technology and programs that create personalized wellness development using technology, gamification, network effects and other strategies that help veterans in rehabilitiation.  </p>
+                                        </div>
+                                    </div>
+                                    <div class="row"> 
+                                        <div class="wide-separator-line"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 col-sm-12 text-center service-year black-text">
+                                            <strong>16 veterans with 4 full time support staff on site occupy the 26,000 sq. ft private estate</strong>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- end tab content -->
+                                <!-- tab content -->
+                                <div class="tab-pane fade in active" id="tab_sec3">
+                                    <div class="row"> 
+                                        <div class="col-md-6 col-sm-12 text-left gray-text">
+                                            <p class="text-large margin-right-ten"> We have partnered with leading edge scientists, professors, veterans and other professionals to build a world class veterans rehab facility and program.</p>
+                                        </div>
+                                        <div class="col-md-6 col-sm-12 text-left text-med gray-text">
+                                            <p class="text-uppercase">Join us and inquire about our <a href="/partners">partnership programs</a>.We seek to partner with existing and respected entrepreneurs/companies to help with operational, staffing and certification requirements.
+</p>
+                                            <p>Our partner network is growing and look forward to learning about other organizations and people doing amazing things that can impact our committement to supporting and getting our veterans 100% healthy.</p>
+                                        </div>
+                                    </div>
+                                    <div class="row"> 
+                                        <div class="wide-separator-line"></div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12 col-sm-12 text-center service-year black-text">
+                                            <strong> </strong>
+                                        </div>
+                                    </div>
+                                </div>
+                                <!-- end tab content -->
+                            </div>
+                            <!-- end tab content section -->
+                        </div>
+                        <!-- end tab -->
+                    </div>
+                </div>
+            </div>
+        </section>
+    
+    
+    <script type="text/javascript" charset="utf-8">
+        	$(document).ready(function(){
+        		$('#email').click(function(){
+        			$(this).attr("placeholder","");
+        		});
+        		
+        		$('#pagesubmit').hide();
+        		$('#pagesubmit').removeClass('hidden');
+        		$('#btnsubmit').click(function(){
+        			$('#signupform').submit();
+        		});
+        		$('#signupform').submit(function(){
+        			
+        			var email = $('#email').val();
+        			var user_ip = $('#user_ip').val();
+        			var indexof = email.indexOf("@");
+        			var name = email.slice(0,indexof);
+        			var domain = $('#domain').val(); 
+        			
+        			
+        			if(email==''){
+        				alert('Email is Required.');
+        				$('#email').focus();
+        				return false;
+        			}else if(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i.test(email)==false){
+        				alert('Please enter a valid email address.');
+        				$('#email').focus();
+        				return false;
+        			}else{
+        				
+        				$("#signupform").hide();	
+        				$('#response_wait').show();
+        				$("#pagesubmit").slideDown('normal');
+        				
+        			//DOMAINDI LEADS		
+        			jQuery.ajax({
+        				type: "post",url: "http://www.api.contrib.com/forms/saveleads",
+        				data: {'email':email, 'domain':domain,'user_ip':user_ip},
+        				success: function(res){
+        					$('#response_wait').hide();
+        					console.log(res);
+        					
+        					$("#loading").hide();
+        					
+        					if(res.success=='false'){
+        						$('#response').append('<div class="span12 text-center" id="response">Something went wrong. Sorry for the inconvenience.</div>');
+        					}else if(res.success=='success'){
+        						
+        						
+        						$('#pemail').val(email);
+        						$('#response').css('display','block');
+        						
+        					}else{
+        						$('#response').append('<div class="span12 text-center" id="response">'+res.success+'<br><br>Visit our <a href="http://www.contrib.com/brand/details/'+domain+'" target="_blank">'+domain.toUpperCase()+' brand page</a> for further details. <br><br>Thank you! <br><br></div>');
+        					}
+        					
+        				}});	
+        			
+        				// SALESFORCE LEAD
+        				$.post("http://www.manage.vnoc.com/salesforce/addlead",
+        				{
+        					'firstName':name,
+        					'lastName':name,
+        					'title':'',
+        					'email':email,
+        					'phone':'',
+        					'street':'',
+        					'city':'',
+        					'country':'',
+        					'state':'',
+        					'zip':'',
+        					'domain':domain,
+        					'form_type':'Contrib Lead Template'
+        					
+        				},function(data2){
+        					console.log(data2);
+        				}
+        				);
+        				
+        				
+        				return false;
+        				
+        			}
+        			
+        			return false;
+
+        		});
+
+        });
+
+</script>
+                    <!-- end form -->
+                </div>
+            </div>
+        </section>
+        <!-- end contact us section -->
+       <? include 'footer.php'; ?> 
+
+        <!-- javascript libraries --> 
+        <!--<script type="text/javascript" src="js/jquery.min.js"></script>-->
+        <script type="text/javascript" src="js/modernizr.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script> 
+        <script type="text/javascript" src="js/bootstrap-hover-dropdown.js"></script>
+        <script type="text/javascript" src="js/jquery.easing.1.3.js"></script> 
+        <script type="text/javascript" src="js/skrollr.min.js"></script>  
+        <script type="text/javascript" src="js/jquery.viewport.mini.js"></script>
+        <script type="text/javascript" src="js/smooth-scroll.js"></script>
+        <!-- animation -->
+        <script type="text/javascript" src="js/wow.min.js"></script>
+        <!-- page scroll -->
+        <script type="text/javascript" src="js/page-scroll.js"></script>
+        <!-- easy piechart-->
+        <script type="text/javascript" src="js/jquery.easypiechart.js"></script>
+        <!-- parallax -->
+        <script type="text/javascript" src="js/jquery.parallax-1.1.3.js"></script>
+        <!--portfolio with shorting tab --> 
+        <script type="text/javascript" src="js/jquery.isotope.min.js"></script> 
+        <!-- owl slider  -->
+        <script type="text/javascript" src="js/owl.carousel.min.js"></script>
+        <!-- magnific popup  -->
+        <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+        <script type="text/javascript" src="js/popup-gallery.js"></script>
+        <!-- text effect  -->
+        <script type="text/javascript" src="js/text-effect.js"></script>
+        <!-- revolution slider  -->
+        <script type="text/javascript" src="js/jquery.tools.min.js"></script>
+        <script type="text/javascript" src="js/jquery.revolution.js"></script>
+        <!-- counter  -->
+        <script type="text/javascript" src="js/counter.js"></script>
+        <!-- fit videos  -->
+        <script type="text/javascript" src="js/jquery.fitvids.js"></script>
+        <!-- imagesloaded  -->
+        <script type="text/javascript" src="js/imagesloaded.pkgd.min.js"></script>
+        <!-- one page navigation-->
+        <script type="text/javascript" src="js/one-page-main.js"></script>
+        <!-- setting --> 
+        <script type="text/javascript" src="js/main.js"></script>
+    </body>
+</html>
