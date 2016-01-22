d09a142f62e41d1d57ff359a2f052426513b12eb
Larry C. Parba <larz.parba@gmail.com>
2016-01-22 13:42:14 +0800

    Includes files for Modeltable.com

---
+++ includes/config-framework.php
@@ -0,0 +1,316 @@
+<?php
+    $domain =   "modeltable.com";
+    $domainid = "22707";
+    $memberid = "8";
+    $title = "Welcome to ModelTable.com";
+    $logo =  "http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-modeltable-6.png";
+    $description = "Models helping set The Vibe for your Venue. Book your table now.";
+    $account_ga = "UA-29828251-6";
+    $description = stripslashes(str_replace('\n','<br>',$description));
+    $background_image = "http://rdbuploads.s3.amazonaws.com/backgrounds/bg-modeltable.jpg";
+    $fb_page =  "";
+    $twitter = "";
+    $bottom_text  = "";
+    $forsale = "1";
+    $forsaledefault = "1";
+	$forsaletext = "This domain belongs to the Global Ventures network. We have interesting opportunities for work, sponsors and partnerships.";
+    $footer_banner = "";
+    $domain_affiliate_link = "http://referrals.contrib.com/idevaffiliate.php?id=881&url=http://www.contrib.com/signup/firststep?domain=modeltable.com";
+    $affiliate_id = "881";
+    $additional_html = "CQk=";
+    $piwik_id = '5485';
+    $related_domains = array (
+  0 => 
+  array (
+    'domain_name' => 'talent.holdings',
+    'domain_id' => '21952',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-Talent-Holdings1.png',
+    'title' => 'Talent.holdings',
+    'slug' => 'fashion-style',
+  ),
+  1 => 
+  array (
+    'domain_name' => 'talentcart.com',
+    'domain_id' => '21816',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-TalentCart1.png',
+    'title' => 'Welcome to talentcart.com',
+    'slug' => 'fashion-style',
+  ),
+  2 => 
+  array (
+    'domain_name' => 'talentpick.com',
+    'domain_id' => '23499',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-TalentPick1.png',
+    'title' => 'Welcome to talentpick.com',
+    'slug' => 'fashion-style',
+  ),
+  3 => 
+  array (
+    'domain_name' => 'talentholdings.com',
+    'domain_id' => '16101',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-talentholdings1.png',
+    'title' => 'Welcome to talentholdings.com',
+    'slug' => 'fashion-style',
+  ),
+  4 => 
+  array (
+    'domain_name' => 'modelcorp.com',
+    'domain_id' => '606',
+    'logo' => 'http://contrib.com/uploads/logo/modelcorp.png',
+    'title' => 'Modelcorp.com',
+    'slug' => 'fashion-style',
+  ),
+  5 => 
+  array (
+    'domain_name' => 'talentscore.com',
+    'domain_id' => '6887',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-talentscore1.png',
+    'title' => 'Welcome to talentscore.com',
+    'slug' => 'fashion-style',
+  ),
+  6 => 
+  array (
+    'domain_name' => 'modelmayhan.com',
+    'domain_id' => '2870',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-ModelMayHan1.png',
+    'title' => 'Welcome to modelmayhan.com',
+    'slug' => 'fashion-style',
+  ),
+  7 => 
+  array (
+    'domain_name' => 'perfectextras.com',
+    'domain_id' => '899',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/marvinpogi/logo-perfectxtras-8.png',
+    'title' => '',
+    'slug' => 'fashion-style',
+  ),
+  8 => 
+  array (
+    'domain_name' => 'thefashionconnection.com',
+    'domain_id' => '23847',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-theFashionConnection1.png',
+    'title' => 'Thefashionconnection.com',
+    'slug' => 'fashion-style',
+  ),
+  9 => 
+  array (
+    'domain_name' => 'fashiondesign.com',
+    'domain_id' => '1298',
+    'logo' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-fashiondesign1.png',
+    'title' => 'Fashiondesign.com',
+    'slug' => 'fashion-style',
+  ),
+);
+    $fund_campaigns = array (
+  0 => 
+  array (
+    'post_title' => 'Acting.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => NULL,
+    'campaign_author' => NULL,
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/acting-com/',
+    'post_name' => 'acting-com',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/01/acting.com_.png',
+    'post_content' => 'Acting.com allows actors and people in the entertainment industry to put up their profile to get casting audition jobs and opportunities. It also allows companies in the Production industry to...',
+  ),
+  1 => 
+  array (
+    'post_title' => 'CodeChallenge.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => NULL,
+    'campaign_author' => NULL,
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/codechallenge-com/',
+    'post_name' => 'codechallenge-com',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/01/codechallenge.png',
+    'post_content' => '<h4>Join CodeChallenge.com</h4>
+<h4><strong>Are you an entrepreneur or want to work for a great online business?</strong></h4>
+*Select your niche.
+*Get mentorship and access to premium online...',
+  ),
+  2 => 
+  array (
+    'post_title' => 'Cookboard.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'Florida',
+    'campaign_author' => 'Maai Florendo',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/cookboard-com/',
+    'post_name' => 'cookboard-com',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/11/logo-CookBoard-2.png',
+    'post_content' => 'Your #1 Local Food Marketplace community! Join us as we launch it Dec 2014!
+
+Where local chefs create cook boards for availability to the local foodie community\\n\\n
+
+In depth...',
+  ),
+  3 => 
+  array (
+    'post_title' => 'CoWork.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'Delray Beach, Florida',
+    'campaign_author' => 'CoWork',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/cowork-com-virtual-opportunities-to-do-amazing-things/',
+    'post_name' => 'cowork-com-virtual-opportunities-to-do-amazing-things',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/02/4743596194.jpg',
+    'post_content' => '<a href="http://www.cowork.com">Cowork.com</a>
+
+CoWork.com is an online venture creation network that works with other like minded professionals building awesome companies, projects and...',
+  ),
+  4 => 
+  array (
+    'post_title' => 'Helping Women  Discover their Talents and Sustain a living.',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'BAMENDA.CAMEROON',
+    'campaign_author' => 'KEKORTE (Struggling Women)',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/helping-women-discover-their-talents-and-sustain-a-living/',
+    'post_name' => 'helping-women-discover-their-talents-and-sustain-a-living',
+    'logo' => '',
+    'post_content' => 'We are women from a small village in Cameroon from the city of Bamenda. The name of our Initiative group is KEKORTE  (Struggling Women) and our gold is to promote women and also help the...',
+  ),
+  5 => 
+  array (
+    'post_title' => 'iChallenge.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'BAMENDA.CAMEROON',
+    'campaign_author' => 'KEKORTE (Struggling Women)',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/ichallenge-com/',
+    'post_name' => 'ichallenge-com',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/01/3616845592.jpg',
+    'post_content' => 'Ichallenge.com is a gamification challenge framework that allows you to join a challenge or sponsor a challenge without monetary involvement.
+
+&nbsp;',
+  ),
+  6 => 
+  array (
+    'post_title' => 'iContent.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'Delray Beach, Florida',
+    'campaign_author' => 'iContent, LLC',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/icontent-com/',
+    'post_name' => 'icontent-com',
+    'logo' => 'http://ifund.com/wp-content/uploads/2014/03/icontent-logo.png',
+    'post_content' => '<p>iContent is a xml application that connects all your content into an integrated and intelligent system that learns and earns while you focus on creating amazing audio, visual and contextual...',
+  ),
+  7 => 
+  array (
+    'post_title' => 'Linked.com',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'Delray Beach, Florida',
+    'campaign_author' => 'iContent, LLC',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/linked-com/',
+    'post_name' => 'linked-com',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/03/Linked_com-linked_com.png',
+    'post_content' => 'Linking people, skills and opportunities to create the worlds largest crowd commerce business creation system. Be a part of Linked.com and get equity shares for your donations.
+
+<img...',
+  ),
+  8 => 
+  array (
+    'post_title' => 'MBAChallenge',
+    'campaign_goal' => '10000.00',
+    'campaign_location' => 'Delray Beach, FL',
+    'campaign_author' => 'MBACHallenge',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/mbachallenge/',
+    'post_name' => 'mbachallenge',
+    'logo' => 'http://ifund.com/wp-content/uploads/2015/06/IMG_1404.jpg',
+    'post_content' => 'MBAChallenge is a leading skills based business game created to challenge college educated people with street smart professionals.',
+  ),
+  9 => 
+  array (
+    'post_title' => 'Micro Markets',
+    'campaign_goal' => '100000.00',
+    'campaign_location' => 'Delray Beach, FL',
+    'campaign_author' => 'MBACHallenge',
+    'campaign_type' => 'donation',
+    'permalink' => 'http://ifund.com/campaigns/micro-markets/',
+    'post_name' => 'micro-markets',
+    'logo' => 'http://ifund.com/wp-content/uploads/edd/2014/01/micromarkets1.png',
+    'post_content' => '<a href="http://ifund.com/wp-content/uploads/edd/2014/01/logo-micromarkets.png"><img alt="logo-micromarkets" src="http://ifund.com/wp-content/uploads/edd/2014/01/logo-micromarkets-300x44.png"...',
+  ),
+);
+    $partners = array (
+  0 => 
+  array (
+    'partner_id' => '38350',
+    'summary' => 'Contribute your Skills, Services, Apps, Contacts or Capital, part-time. Help a team of other passionate people doing amazing things. Come learn and earn! Amazing things happen with the right people, technology, business model, resources and passion. Earn equity ownership, Learn from other great people, Lead a venture and distribute up to 2% of the shares to your charity.',
+    'company_name' => 'Contrib.com',
+    'domain' => 'modeltable.com',
+    'url' => 'http://www.contrib.com',
+    'approved_by' => '10',
+    'date_applied' => '2014-11-04 06:44:15',
+    'approved' => '1',
+    'image' => 'http://d2qcctj8epnr7y.cloudfront.net/images/2013/logo-contrib-174x35.png',
+    'member_id' => '7',
+    'approved_status' => 'approved',
+    'slug' => 'contrib-modeltable-com',
+    'type' => 'Added Value Marketing Partnerships',
+    'partner_views' => '0',
+    'link_type' => 'link',
+    'code' => NULL,
+    'is_custom' => '0',
+    'description' => 'We are a community of Entrepreneurs, Developers, Designers, Marketers and Specialists from around the world building, managing and monetizing virtual businesses on premium domains for equity and cash grants.',
+    'exchange_url' => NULL,
+    'ld_id' => '0',
+    'ld_url' => NULL,
+    'in_equity' => '0',
+  ),
+  1 => 
+  array (
+    'partner_id' => '14389',
+    'summary' => 'Global Ventures owns a premium network of 20,000 websites and powerful tools to help you build successful companies quickly. Some of the things we offer you include a great domain name with targeted traffic, unique business model, equity ownership, and flexible, performance based compensation. You just need to bring your knowledge, passion and work smart.',
+    'company_name' => 'GlobalVentures.com',
+    'domain' => 'modeltable.com',
+    'url' => 'http://globalventures.com',
+    'approved_by' => '10',
+    'date_applied' => '2014-11-04 06:34:13',
+    'approved' => '1',
+    'image' => 'http://d2qcctj8epnr7y.cloudfront.net/images/lucille/logo-gv-re283x35.png',
+    'member_id' => '7',
+    'approved_status' => 'approved',
+    'slug' => 'globalventures-modeltable-com',
+    'type' => 'Added Value Marketing Partnerships',
+    'partner_views' => '0',
+    'link_type' => 'link',
+    'code' => NULL,
+    'is_custom' => '0',
+    'description' => 'With over 17 years of internet experience, we built a network of over 20,000 websites and created dozens of successful businesses. We would love to work on the next cutting-edge projects with great companies and talented people.',
+    'exchange_url' => NULL,
+    'ld_id' => '0',
+    'ld_url' => NULL,
+    'in_equity' => '0',
+  ),
+  2 => 
+  array (
+    'partner_id' => '48730',
+    'summary' => 'Talent Direct, formerly known as Modelstore, started out as an upscale national event and promotional company servicing all markets in the USA and has since become a full-service SAG Aftra Franchised Talent Agency located in Boca Raton, Florida and also practicing in Atlanta, GA. Talent Direct is an internationally recognized full-service agency representing union and non-union youth and adults. We are dedicated to providing the casting industry with unparalleled service with talent in every facet of the Entertainment Industry - See more at: http://talentdirect.com/#sthash.NYOoGl1z.dpuf',
+    'company_name' => 'TalentDirect.com',
+    'domain' => 'modeltable.com',
+    'url' => 'http://referrals.contrib.com/idevaffiliate.php?id=881_15_1_50&idev_domain=modeltable.com&idev_product=17',
+    'approved_by' => '12',
+    'date_applied' => '2015-10-06 06:54:20',
+    'approved' => '1',
+    'image' => 'http://rdbuploads.s3.amazonaws.com/logos/logo-tda.png',
+    'member_id' => '0',
+    'approved_status' => 'approved',
+    'slug' => NULL,
+    'type' => 'Added Value Marketing Partnerships',
+    'partner_views' => '0',
+    'link_type' => 'link',
+    'code' => '',
+    'is_custom' => '1',
+    'description' => 'Talent Direct Agency - Florida and South Florida - A Premium Talent Agency in Florida and South Florida - ',
+    'exchange_url' => 'talentidrect.com',
+    'ld_id' => '0',
+    'ld_url' => NULL,
+    'in_equity' => '0',
+  ),
+);
+?>
